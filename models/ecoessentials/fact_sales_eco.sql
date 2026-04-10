ebcf77a932219748b654e12658cc96627bbdc734
Zach Speck <Zach.Speck@usu.edu>
2026-04-10 03:23:53 +0000

    Deliverable #3: Testing & Scheduling

--- models/ecoessentials/fact_sales_eco.sql
+++ models/ecoessentials/fact_sales_eco.sql
@@ -1,9 +1,14 @@
 {{ config(materialized = 'table', schema = 'dw_ecoessentials') }}
-
-select
-    {{ dbt_utils.generate_surrogate_key(['ol.order_id', 'ol.product_id']) }} as sales_key,
-    c.customer_key, p.product_key, dc.campaign_key, d.date_key,
-    cast(o.order_id as varchar) as order_id, ol.quantity, ol.discount, ol.price_after_discount
+select distinct
+    {{ dbt_utils.generate_surrogate_key(['ol.order_id', 'ol.product_id', 'ol.quantity', 'ol.price_after_discount']) }} as sales_key,
+    c.customer_key, 
+    p.product_key, 
+    dc.campaign_key, 
+    d.date_key,
+    cast(o.order_id as varchar) as order_id, 
+    ol.quantity, 
+    ol.discount, 
+    ol.price_after_discount
 from {{ source('rds_source', 'ORDER_LINE') }} ol
 inner join {{ source('rds_source', 'ORDER') }} o on ol.order_id = o.order_id
 inner join {{ ref('dim_customer_eco') }} c on cast(o.customer_id as varchar) = c.customer_id
